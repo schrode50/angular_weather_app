d6050bba64d48a74c0aa2bdf8992ded418f445fb
Stephen Schroeder <schrode50@gmail.com>
2017-07-11 15:42:36 -0700

    small tweaks

--- Client/home/home.js
+++ Client/home/home.js
@@ -6,7 +6,7 @@ angular.module("WeatherAngular.home", [])
   $scope.submit = function(city){
      weatherinfo.getWeather(city)
      .then(function(dataFromServer){
-       console.log("city is ---->", dataFromServer.data);
+       console.log("city is ---->", dataFromServer.name);
        console.log("temp is --->", dataFromServer.data.main.temp);
        $scope.data = dataFromServer.data.weather;
        $scope.temp = Math.floor(dataFromServer.data.main.temp * 9/5 - 459.67) + " " + "\xB0" + "F";
--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ app.get('/api/location', function(req, res){
   var city = req.query.city.replace(/\s/g, '');
   var url = {
     host: 'api.openweathermap.org',
-    path: "/data/2.5/weather?q=" + city + "&appid=e42d32af9555a899db7106b67e9e5aae"
+    path: "/data/2.5/weather?q=" + city + "&appid=59ec812db281590db55541ce3de26508"
   };
   http.get(url, function(response){
     var bodyChunks = "";
@@ -23,7 +23,8 @@ app.get('/api/location', function(req, res){
   });
 });
 
+// let port = process.env.PORT || 4000;
 app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), function(){
-  console.log("We are listening");
+  console.log("listening on " + app.get('port'));
 })
